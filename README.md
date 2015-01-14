cbd93dd143511b998e3f4c2f7a80b9d5dd4dcaae
Brian Auron <brianauron@gmail.com>
2015-01-14 15:49:08 -0600

    Small style changes in text

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HorseFinder
-Twitter bot that looks for horses and retweets them. Checks for DMs which must be shares to tweets and retweets those.
+- Twitter bot that looks for horses and retweets them.
+- Checks for DMs which must be shares to tweets and retweets those.
--- annoying_stuff.py
+++ annoying_stuff.py
@@ -7,12 +7,12 @@
 #
 #  Creation Date : 14-01-2015
 #
-#  Last Modified : Wed 14 Jan 2015 03:01:37 PM CST
+#  Last Modified : Wed 14 Jan 2015 03:47:36 PM CST
 #
 #  Created By : Brian Auron
 #
 # ========================================
 
 words = ['My Little Pony',
-                  'MyLittlePony',
-                  'Pony']
+         'MyLittlePony',
+         'Pony']
